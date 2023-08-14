b51aed5b411da8e5051cf918b28426109187ad80
Shubham Gaikwad <shubhamtechgeek@gmail.com>
2023-08-14 14:15:16 +0530

    Navigation-from tailwind CSS

--- codecartfrontend/src/App.js
+++ codecartfrontend/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './customer/components/navigation/navigation';
+
 
 function App() {
   return (
-    <div className="App">
-      
+    
+    <div className="">
+      <Navigation/>
+      <div>
+        Home
+      </div>
     </div>
   );
 }
