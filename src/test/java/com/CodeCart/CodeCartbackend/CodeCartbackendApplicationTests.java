c52aec51b2b1ae1c2ea1576d724e71ed5b0344c9
Varun Tandalaskar <varuntandalaskar1855@gmail.com>
2023-08-14 13:52:42 +0530

    First Backend commit

---
+++ src/test/java/com/CodeCart/CodeCartbackend/CodeCartbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CodeCart.CodeCartbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCartbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
