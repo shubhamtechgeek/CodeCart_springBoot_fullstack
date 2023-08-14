31cdc9de57a54f2fc9eda492c6f2cf06085b0eb5
Varun Tandalaskar <varuntandalaskar1855@gmail.com>
2023-08-14 18:56:21 +0530

    Added Jwt Authentication for APi's

---
+++ src/main/java/com/CodeCart/CodeCartbackend/Config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.CodeCart.CodeCartbackend.Config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "vjadvagvvcuywbivbbvbsvbe";
+    public static final String JWT_HEADER="Authorization";
+}
